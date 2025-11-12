1b306892b86ed3d0411bc33e86cc0358875c992d
AW12 <AW12@localhost.localdomain>
2025-11-12 08:38:32 +0800

    Applying changes to network_monitoring/urls.py

--- network_monitor/urls.py
+++ network_monitor/urls.py
@@ -15,21 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-<<<<<<< HEAD
+
 from django.urls import path, re_path, include
 from django.views.generic import TemplateView
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('monitoring.urls')),
-
-    # Catch-all pattern to serve the React frontend
-    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
-=======
-from django.urls import path, include
 from monitoring.admin import custom_admin_site
 
 urlpatterns = [
     path('admin/', custom_admin_site.urls),
->>>>>>> origin/main
+    path('api/', include('monitoring.urls')),
+
+    # Catch-all pattern to serve the React frontend
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
